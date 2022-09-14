5e159e848556360962a326ed15b86cb9a905a207
DESKTOP-FKHPESP\JinSeong <mac9692@nate.com>
2022-09-14 22:11:20 +0900

    20220914 JUnit5 study

--- src/test/java/me/jinseong/springtestdemo/StudyTest.java
+++ src/test/java/me/jinseong/springtestdemo/StudyTest.java
@@ -1,10 +1,14 @@
 package me.jinseong.springtestdemo;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
 
 import java.time.Duration;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 // 언더바를 공백으로 치환해주는 설정
 //@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -33,7 +37,6 @@ class StudyTest {
     void afterEach() {
         System.out.println("StudyTest.afterEach");
     }
-
 
 
     @Test
@@ -81,5 +84,85 @@ class StudyTest {
             Thread.sleep(300);
         });
         System.out.println("StudyTest.timeout_test");
+    }
+
+    @Test
+    @DisplayName("6-1. 상황에 맞는 테스트 실행")
+    void assume_test() {
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println(test_env);
+
+//        첫 번째 방법
+        assumeTrue("LOCAL".equalsIgnoreCase(test_env));
+
+//        두 번째 방법
+        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+
+        });
+
+        assumingThat("TEST".equalsIgnoreCase(test_env), () -> {
+
+        });
+
+        Study actual = new Study(10);
+        assertThat(actual.getLimit() < 0);
+    }
+
+    @Test
+    @EnabledOnOs({OS.LINUX, OS.MAC})
+    @DisplayName("6-2. @EnabledOnOs OS에 맞는 테스트 실행")
+    void os_test() {
+        Study actual = new Study(10);
+        assertThat(actual.getLimit() < 0);
+    }
+
+    @Test
+    @EnabledOnJre({JRE.JAVA_8, JRE.JAVA_9})
+    @DisplayName("6-2. @EnabledOnJre JRE에 맞는 테스트 실행")
+    void jre_test() {
+        Study actual = new Study(10);
+        assertThat(actual.getLimit() < 0);
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "TEST")
+    @DisplayName("6-2. @EnabledIfEnvironmentVariables 환경변수에 맞는 테스트 실행")
+    void environment_variables_test() {
+        Study actual = new Study(10);
+        assertThat(actual.getLimit() < 0);
+    }
+
+
+    //    테스트 태깅
+//    테스트 실행 시간이 긴 지, 짧은 지
+//    모듈 별로도 할 수 있고
+//    단위 테스트인 지, 통합 테스트인 지
+    @Test
+    @Tag("fast")
+    @DisplayName("7-1 @Tag 테스트")
+    void tag_test1() {
+//        Edit configurations 에서 Hint 타입을 tags 로 변경 후 태그값 설정
+        System.out.println("StudyTest.tag_test");
+    }
+
+    @Test
+    @Tag("slow")
+    @DisplayName("7-1 @Tag 테스트")
+    void tag_test2() {
+//        Edit configurations 에서 Hint 타입을 tags 로 변경 후 태그값 설정
+        System.out.println("StudyTest.tag_test2");
+    }
+
+//    @Tag 어노테이션을 사용했을 때 리터럴을 사용하기 때문에 type safe 하지 않다.
+    @FastTest
+    @DisplayName("8-1 커스텀 어노테이션 테스트")
+    void custom_annotations_test1() {
+        System.out.println("StudyTest.custom_annotations_test1");
+    }
+
+    @SlowTest
+    @DisplayName("8-1 커스텀 어노테이션 테스트")
+    void custom_annotations_test2() {
+        System.out.println("StudyTest.custom_annotations_test2");
     }
 }
